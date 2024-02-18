d25474835dc6b8a51039a923fef85205fa797bcd
stormbreakermerika <benjamin.t.kaiser@outlook.com>
2024-02-17 21:24:29 -0700

    Add project files.

---
+++ FSharpConsoleAppTutorial/Program.fs
@@ -0,0 +1,27 @@
+﻿module PatternMatching =
+
+    /// A record for a person's first and last name
+    type Person = {
+        First : string
+        Last  : string
+    }
+
+    /// A Discriminated Union of 3 different kinds of employees
+    type Employee =
+        | Engineer of engineer: Person
+        | Manager of manager: Person * reports: List<Employee>
+        | Executive of executive: Person * reports: List<Employee> * assistant: Employee
+
+    /// Count everyone underneath the employee in the management hierarchy,
+    /// including the employee. The matches bind names to the properties
+    /// of the cases so that those names can be used inside the match branches.
+    /// Note that the names used for binding do not need to be the same as the
+    /// names given in the DU definition above.
+    let rec countReports(emp : Employee) =
+        1 + match emp with
+            | Engineer(person) ->
+                0
+            | Manager(person, reports) ->
+                reports |> List.sumBy countReports
+            | Executive(person, reports, assistant) ->
+                (reports |> List.sumBy countReports) + countReports assistant
